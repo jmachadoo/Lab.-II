e0b1fc5cbfb2a65dfc74fbe6aa425d20bc3bc78a
João Vitor Fernandes Machado <88752151+jmachadoo@users.noreply.github.com>
2022-06-22 23:53:44 -0300

    Add files via upload

---
+++ Locadora1/banco.sql
@@ -0,0 +1,24 @@
+/*create database locadora;
+use locadora;
+
+create table carro(
+    modelo VARCHAR(50),
+    cor VARCHAR(50),
+    ano INTEGER,
+    potencia FLOAT,
+    marca VARCHAR(50),
+    preco FLOAT
+);
+
+create table cliente(
+    cpf INTEGER,
+    nome VARCHAR(50)
+)
+;
+create table funcionario(
+    nome VARCHAR(50),
+    idade INTEGER,
+    funcao VARCHAR(50)
+)*/
+
+select * from cliente;
